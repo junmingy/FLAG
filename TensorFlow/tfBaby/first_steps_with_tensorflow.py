bdf94252eb6ef08d4d6bb21ab3f72c2e64d56fa5
phd <huid.peng@gmail.com>
2018-10-08 18:17:52 +0800

    Backhome continuting

--- TensorFlow/tfBaby/first_steps_with_tensorflow.py
+++ TensorFlow/tfBaby/first_steps_with_tensorflow.py
@@ -23,11 +23,13 @@ pd.options.display.float_format = '{:.1f}'.format
 
 california_housing_dataframe = pd.read_csv("california_housing_train.csv", sep=",")
 
-#california_housing_dataframe = california_housing_dataframe.reindex(np.random.permutation(california_housing_dataframe.index))
+california_housing_dataframe = california_housing_dataframe.reindex(
+         np.random.permutation(california_housing_dataframe.index))
 california_housing_dataframe["median_house_value"] /= 1000.0
-print(california_housing_dataframe)
 
-print(california_housing_dataframe.describe())
+#print(california_housing_dataframe.index)
+#print(california_housing_dataframe)
+#print(california_housing_dataframe.describe())
 
 # Step 1
 # Define the input feature: total rooms
@@ -47,7 +49,6 @@ my_optimizer = tf.contrib.estimator.clip_gradients_by_norm(my_optimizer, 5.0)
 
 # Configure the linear regression model with our feature columns and optimizer.
 # Set a learning rate of 0.0000001 for Gradient Descent.
-
 linear_regressor = tf.estimator.LinearRegressor(feature_columns=feature_columns, optimizer=my_optimizer)
 
 def my_input_fn(features, tatgets, batch_size=1, shuffle=True, num_epochs=None):
@@ -98,7 +99,7 @@ root_mean_squared_error = math.sqrt(mean_squared_error)
 print("Mean Squared Error (on training data): %0.3f" % mean_squared_error)
 print("Root Mean Squared Error (on training data): %0.3f" % root_mean_squared_error)
 
-calibration_data = pd.DataFrame()
-calibration_data["predictions"] = pd.Series(predictions)
-calibration_data["targets"] = pd.Series(targets)
-calibration_data.describe()
+# calibration_data = pd.DataFrame()
+# calibration_data["predictions"] = pd.Series(predictions)
+# calibration_data["targets"] = pd.Series(targets)
+# calibration_data.describe()
