769e12627cd92defaef229dcd7dc714b2f350d76
phd <huid.peng@gmail.com>
2018-10-09 18:43:07 +0800

    Scatter Linear Regression

--- TensorFlow/tfBaby/synthetic_features_and_outliers.py
+++ TensorFlow/tfBaby/synthetic_features_and_outliers.py
@@ -27,29 +27,6 @@ california_housing_dataframe = california_housing_dataframe.reindex(
          np.random.permutation(california_housing_dataframe.index))
 california_housing_dataframe["median_house_value"] /= 1000.0
 
-#print(california_housing_dataframe.index)
-#print(california_housing_dataframe)
-#print(california_housing_dataframe.describe())
-
-# # Step 1
-# # Define the input feature: total rooms
-# my_feature = california_housing_dataframe[["total_rooms"]]
-#
-# # Configure a numeric feature column for total_rooms
-# feature_columns = [tf.feature_column.numeric_column("total_rooms")]
-#
-# # Step 2
-# # Define the label
-# targets = california_housing_dataframe["median_house_value"]
-#
-# # Step 3
-# # Use gradient descent as the optimizer for training the model
-# my_optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.0000001)
-# my_optimizer = tf.contrib.estimator.clip_gradients_by_norm(my_optimizer, 5.0)
-#
-# # Configure the linear regression model with our feature columns and optimizer.
-# # Set a learning rate of 0.0000001 for Gradient Descent.
-# linear_regressor = tf.estimator.LinearRegressor(feature_columns=feature_columns, optimizer=my_optimizer)
 
 def my_input_fn(features, targets, batch_size=1, shuffle=True, num_epochs=None):
     """Trains a linear regression model of one feature
@@ -78,73 +55,6 @@ def my_input_fn(features, targets, batch_size=1, shuffle=True, num_epochs=None):
     features, labels = ds.make_one_shot_iterator().get_next()
     return features, labels
 
-# _ = linear_regressor.train(input_fn=lambda :my_input_fn(my_feature, targets),
-#                            steps=100)
-#
-# # Create an input function for predictions
-# # Note: Since we're making just one prediction for each example, we don't
-# # need to repeat or shuffle the data here
-#
-# prediction_input_fn = lambda: my_input_fn(my_feature, targets, num_epochs=1, shuffle=False)
-#
-# # Call predict() on the linear_regressor to make predictions
-# predictions = linear_regressor.predict(input_fn=prediction_input_fn)
-#
-# # Format predictions as a NumPy array, so we can calculate error metrics
-# predictions = np.array([item['predictions'][0] for item in predictions])
-# # what are the predictions mean?
-# # print(predictions)
-# # print(targets[0])
-#
-# # Print Mean Squared Error and Root Mean Squared Error
-# mean_squared_error = metrics.mean_squared_error(predictions, targets)
-# root_mean_squared_error = math.sqrt(mean_squared_error)
-# print("Mean Squared Error (on training data): %0.3f" % mean_squared_error)
-# print("Root Mean Squared Error (on training data): %0.3f" % root_mean_squared_error)
-#
-# min_house_value = california_housing_dataframe["median_house_value"].min()
-# max_house_value = california_housing_dataframe["median_house_value"].max()
-# min_max_difference = max_house_value - min_house_value
-#
-# print("Min. Median House Value: %0.3f" % min_house_value)
-# print("Max. Median House Value: %0.3f" % max_house_value)
-# print("Difference between Min. and Max.: %0.3f" % min_max_difference)
-# print("Root Mean Squared Error: %0.3f" % root_mean_squared_error)
-#
-# calibration_data = pd.DataFrame()
-# calibration_data["predictions"] = pd.Series(predictions)
-# calibration_data["targets"] = pd.Series(targets)
-# # print(calibration_data)
-# print(calibration_data.describe())
-#
-# sample = california_housing_dataframe.sample(n=300)
-#
-# # Get the min and max total_rooms values
-# x_0 = sample["total_rooms"].min()
-# x_1 = sample["total_rooms"].max()
-#
-# # Retrieve the final weight and bias generated during training
-# weight = linear_regressor.get_variable_value('linear/linear_model/total_rooms/weights')[0]
-# bias = linear_regressor.get_variable_value('linear/linear_model/bias_weights')
-#
-# # Get the predicted median_house_values for the min and max total_rooms values
-# y_0 = weight * x_0 + bias
-# y_1 = weight * x_1 + bias
-#
-# # Plot our regression line from (x_0, y_0) to (x_1, y_1)
-# plt.plot([x_0, x_1], [y_0, y_1], c='r')
-#
-# # Label the graph axes
-# plt.ylabel("median_house_value")
-# plt.xlabel("total_rooms")
-#
-# # Plot a scatter plot from our data sample
-# plt.scatter(sample["total_rooms"], sample["median_house_value"])
-#
-# # Display graph
-# plt.show()
-
-################################################################################
 
 def train_model(learning_rate, steps, batch_size, input_feature="total_rooms"):
     """ Trains a linear regression model of one feature
@@ -242,11 +152,35 @@ def train_model(learning_rate, steps, batch_size, input_feature="total_rooms"):
 
     print("Final RMSE (on training data): %0.2f" % root_mean_squared_error)
 
+    return calibration_data
 
-# Train model
-train_model(learning_rate=0.00001, steps=1000, batch_size=10)
 
-print("")
+california_housing_dataframe["rooms_per_person"] = (
+    california_housing_dataframe["total_rooms"] /
+    california_housing_dataframe["population"]
+)
 
-# Train model with 'population' feature
-train_model(learning_rate=0.00001, steps=1000, batch_size=10, input_feature='population')
+calibration_data = train_model(learning_rate=0.05, steps=500,
+                               batch_size=5, input_feature="rooms_per_person")
+
+plt.figure(figsize=(15, 6))
+plt.subplot(1, 2, 1)
+plt.scatter(calibration_data["predictions"], calibration_data["targets"])
+
+plt.subplot(1, 2, 2)
+_ = california_housing_dataframe["rooms_per_person"].hist()
+plt.show()
+
+california_housing_dataframe["rooms_per_person"] = (
+    california_housing_dataframe["rooms_per_person"].apply(lambda x: min(x, 5))
+)
+_ = california_housing_dataframe["rooms_per_person"].hist()
+plt.show()
+
+
+calibration_data = train_model(learning_rate=0.05, steps=500,
+                               batch_size=5, input_feature="rooms_per_person")
+
+plt.figure()
+_ = plt.scatter(calibration_data["predictions"], calibration_data["targets"])
+plt.show()
