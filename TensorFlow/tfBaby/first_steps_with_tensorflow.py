49a57e599f8d96a0d6d1de2a5b6bbfb541da9600
phdsky <huid.peng@gmail.com>
2018-10-09 08:45:24 +0800

    TF 1st steps almost over

--- TensorFlow/tfBaby/first_steps_with_tensorflow.py
+++ TensorFlow/tfBaby/first_steps_with_tensorflow.py
@@ -92,6 +92,9 @@ predictions = linear_regressor.predict(input_fn=prediction_input_fn)
 
 # Format predictions as a NumPy array, so we can calculate error metrics
 predictions = np.array([item['predictions'][0] for item in predictions])
+# what are the predictions mean?
+# print(predictions)
+# print(targets[0])
 
 # Print Mean Squared Error and Root Mean Squared Error
 mean_squared_error = metrics.mean_squared_error(predictions, targets)
@@ -99,7 +102,30 @@ root_mean_squared_error = math.sqrt(mean_squared_error)
 print("Mean Squared Error (on training data): %0.3f" % mean_squared_error)
 print("Root Mean Squared Error (on training data): %0.3f" % root_mean_squared_error)
 
-# calibration_data = pd.DataFrame()
-# calibration_data["predictions"] = pd.Series(predictions)
-# calibration_data["targets"] = pd.Series(targets)
-# calibration_data.describe()
+min_house_value = california_housing_dataframe["median_house_value"].min()
+max_house_value = california_housing_dataframe["median_house_value"].max()
+min_max_difference = max_house_value - min_house_value
+
+print("Min. Median House Value: %0.3f" % min_house_value)
+print("Max. Median House Value: %0.3f" % max_house_value)
+print("Difference between Min. and Max.: %0.3f" % min_max_difference)
+print("Root Mean Squared Error: %0.3f" % root_mean_squared_error)
+
+calibration_data = pd.DataFrame()
+calibration_data["predictions"] = pd.Series(predictions)
+calibration_data["targets"] = pd.Series(targets)
+# print(calibration_data)
+print(calibration_data.describe())
+
+sample = california_housing_dataframe.sample(n=300)
+
+# Get the min and max total_rooms values
+x_0 = sample["total_rooms"].min()
+x_1 = sample["total_rooms"].max()
+
+# Retrieve the final weight and bias generated during training
+weight = linear_regressor.get_variable_value('linear/linear_model/total_rooms/weights')[0]
+bias = linear_regressor.get_variable_value('linear/linear_model/bias_weights')
+
+# Get the predicted median_house_values for the min and max total_rooms values
+
