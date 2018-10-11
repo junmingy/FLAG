c62bd0dff81eadfee64e354be68526041a32cf55
phdsky <huid.peng@gmail.com>
2018-10-11 21:34:46 +0800

    validating...

--- TensorFlow/tfBaby/validation.py
+++ TensorFlow/tfBaby/validation.py
@@ -4,3 +4,20 @@ import math
 
 from IPython import display
 
+from matplotlib import cm
+from matplotlib import gridspec
+from matplotlib import pyplot as plt
+
+import numpy as np
+import pandas as pd
+
+from sklearn import metrics
+import tensorflow as tf
+from tensorflow.python.data import Dataset
+
+tf.logging.set_verbosity(tf.logging.ERROR)
+pd.options.display.max_rows = 10
+pd.options.display.float_format = '{:.1f}'.format
+
+california_housing_dataframe = pd.read_csv("california_housing_train.csv", sep=",")
+
