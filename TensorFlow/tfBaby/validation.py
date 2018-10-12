b843ac0873e4eab4b6da3f3e9be3c24c5ea03071
phdsky <huid.peng@gmail.com>
2018-10-12 20:54:46 +0800

    Validation dinner

--- TensorFlow/tfBaby/validation.py
+++ TensorFlow/tfBaby/validation.py
@@ -21,3 +21,21 @@ pd.options.display.float_format = '{:.1f}'.format
 
 california_housing_dataframe = pd.read_csv("california_housing_train.csv", sep=",")
 
+# california_housing_dataframe = california_housing_dataframe.reindex(
+#                                np.random.permutation(california_housing_dataframe.index))
+
+def prepeocess_features(california_housing_dataframe):
+    """
+    Prepares input features from California housing data set
+    :param california_housing_dataframe: A Pandas DataFrame expected to contain data
+                                         from the california housing data set
+    :return: A DataFrame that contains the features to be used for the model,
+             including synthetic features
+    """
+    selected_features = california_housing_dataframe[
+        [
+            "latitude",
+            "longitude",
+            ""
+        ]
+    ]
