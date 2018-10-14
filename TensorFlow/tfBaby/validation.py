1fee85dcbdd8cdf1b285ce6329206b2e0fb8d64d
phdsky <huid.peng@gmail.com>
2018-10-14 16:50:02 +0800

    Plot

--- TensorFlow/tfBaby/validation.py
+++ TensorFlow/tfBaby/validation.py
@@ -21,8 +21,8 @@ pd.options.display.float_format = '{:.1f}'.format
 
 california_housing_dataframe = pd.read_csv("california_housing_train.csv", sep=",")
 
-# california_housing_dataframe = california_housing_dataframe.reindex(
-#                                np.random.permutation(california_housing_dataframe.index))
+california_housing_dataframe = california_housing_dataframe.reindex(
+                               np.random.permutation(california_housing_dataframe.index))
 
 def prepeocess_features(california_housing_dataframe):
     """
@@ -84,3 +84,31 @@ validation_targets = preprocess_targets(california_housing_dataframe.tail(5000))
 print(validation_targets.describe())
 
 
+plt.figure(figsize=(13, 8))
+
+ax = plt.subplot(1, 2, 1)
+ax.set_title("Validation Data")
+
+ax.set_autoscaley_on(False)
+ax.set_ylim([32, 43])
+ax.set_autoscalex_on(False)
+ax.set_xlim([-126, -112])
+plt.scatter(validation_examples["longitude"],
+            validation_examples["latitude"],
+            cmap="coolwarm",
+            c=validation_targets["median_house_value"] / validation_targets["median_house_value"].max())
+
+ax = plt.subplot(1, 2, 2)
+ax.set_title("Training Data")
+
+ax.set_autoscaley_on(False)
+ax.set_ylim([32, 43])
+ax.set_autoscalex_on(False)
+ax.set_xlim([-126, -112])
+plt.scatter(training_examples["longitude"],
+            training_examples["latitude"],
+            cmap="coolwarm",
+            c=training_targets["median_house_value"] / training_targets["median_house_value"].max())
+
+_ = plt.plot()
+plt.show()
