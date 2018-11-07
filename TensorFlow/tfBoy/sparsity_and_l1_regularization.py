3858a14520c29db83a8d106e4daf5e5440a3f31c
phdsky <huid.peng@gmail.com>
2018-11-08 00:20:05 +0800

    Lag

--- TensorFlow/tfBoy/sparsity_and_l1_regularization.py
+++ TensorFlow/tfBoy/sparsity_and_l1_regularization.py
@@ -127,7 +127,7 @@ def my_input_fn(features, targets, batch_size=1, shuffle=True, num_epochs=None):
 
 def get_quantile_based_buckets(feature_values, num_buckets):
     quantiles = feature_values.quantile(
-        [(i + 1.) / (num_buckets + 1.)] for i in range(num_buckets)
+        [(i + 1.) / (num_buckets + 1.) for i in range(num_buckets)]
     )
 
     return [quantiles[q] for q in quantiles.keys()]
@@ -154,9 +154,9 @@ def construct_feature_columns():
         boundaries=get_quantile_based_buckets(training_examples["latitude"], 50)
     )
 
-    bucketized_house_median_age = tf.feature_column.bucketized_column(
-        tf.feature_column.numeric_column("house_median_age"),
-        boundaries=get_quantile_based_buckets(training_examples["house_median_age"], 10)
+    bucketized_housing_median_age = tf.feature_column.bucketized_column(
+        tf.feature_column.numeric_column("housing_median_age"),
+        boundaries=get_quantile_based_buckets(training_examples["housing_median_age"], 10)
     )
 
     bucketized_total_rooms = tf.feature_column.bucketized_column(
@@ -164,10 +164,162 @@ def construct_feature_columns():
         boundaries=get_quantile_based_buckets(training_examples["total_rooms"], 10)
     )
 
-    bucketized_total_bedrooms = tf.feature_column.numeric_column(
+    bucketized_total_bedrooms = tf.feature_column.bucketized_column(
         tf.feature_column.numeric_column("total_bedrooms"),
         boundaries=get_quantile_based_buckets(training_examples["total_bedrooms"], 10)
     )
 
+    bucketized_population = tf.feature_column.bucketized_column(
+        tf.feature_column.numeric_column("population"),
+        boundaries=get_quantile_based_buckets(training_examples["population"], 10)
+    )
+
+    bucketized_median_income = tf.feature_column.bucketized_column(
+        tf.feature_column.numeric_column("median_income"),
+        boundaries=get_quantile_based_buckets(training_examples["median_income"], 10)
+    )
+
+    bucketized_rooms_per_person = tf.feature_column.bucketized_column(
+        tf.feature_column.numeric_column("rooms_per_person"),
+        boundaries=get_quantile_based_buckets(training_examples["rooms_per_person"], 10)
+    )
+
+    long_x_lat = tf.feature_column.crossed_column(
+        set([bucketized_longitude, bucketized_latitude]), hash_bucket_size=1000
+    )
+
+    feature_columns = set([
+        long_x_lat,
+        bucketized_longitude,
+        bucketized_latitude,
+        bucketized_housing_median_age,
+        bucketized_total_rooms,
+        bucketized_total_bedrooms,
+        bucketized_population,
+        bucketized_households,
+        bucketized_median_income,
+        bucketized_rooms_per_person
+    ])
+
+    return feature_columns
 
 
+def model_size(estimator):
+    variables = estimator.get_variable_names()
+    size = 0
+    for variable in variables:
+        if not any(x in variable
+                   for x in ['global_step',
+                             'centered_bias_weight',
+                             'bias_weight',
+                             'Ftrl']):
+            size += np.count_nonzero(estimator.get_variable_value(variable))
+    return size
+
+
+def train_linear_classifier_model(
+        learning_rate,
+        regularization_strength,
+        steps,
+        batch_size,
+        feature_columns,
+        training_examples,
+        training_targets,
+        validation_examples,
+        validation_targets
+):
+    """
+    Trains a linear classification model
+
+    In addition to training, this function also prints training progress information,
+    as well as a plot of the training and validation loss over time.
+    :param learning_rate:
+    :param regularization_strength:
+    :param steps:
+    :param batch_size:
+    :param feature_columns:
+    :param training_examples:
+    :param training_targets:
+    :param validation_examples:
+    :param validation_targets:
+    :return:
+    """
+
+    periods = 7
+    steps_per_period = steps / periods
+
+    # Create a linear classifier object
+    my_optimizer = tf.train.FtrlOptimizer(learning_rate=learning_rate, l1_regularization_strength=regularization_strength)
+    my_optimizer = tf.contrib.estimator.clip_gradients_by_norm(my_optimizer, 5.0)
+    linear_classifier = tf.estimator.LinearClassifier(
+        feature_columns=feature_columns,
+        optimizer=my_optimizer
+    )
+
+    # Create input functions
+    training_input_fn = lambda: my_input_fn(training_examples,
+                                            training_targets["median_house_value_is_high"],
+                                            batch_size=batch_size)
+    predict_training_input_fn = lambda: my_input_fn(training_examples,
+                                                    training_targets["median_house_value_is_high"],
+                                                    num_epochs=1,
+                                                    shuffle=False)
+    predict_validation_input_fn = lambda: my_input_fn(validation_examples,
+                                                      validation_targets["median_house_value_is_high"],
+                                                      num_epochs=1,
+                                                      shuffle=False)
+
+    # Train the model, but do so inside a loop so that we can periodically assess
+    # loss metrics.
+    print("Training model...")
+    print("LogLoss (on training data):")
+    training_log_losses = []
+    validation_log_losses = []
+    for period in range(0, periods):
+        # Train the model, starting from the prior state.
+        linear_classifier.train(
+            input_fn=training_input_fn,
+            steps=steps_per_period
+        )
+        # Take a break and compute predictions.
+        training_probabilities = linear_classifier.predict(input_fn=predict_training_input_fn)
+        training_probabilities = np.array([item['probabilities'] for item in training_probabilities])
+
+        validation_probabilities = linear_classifier.predict(input_fn=predict_validation_input_fn)
+        validation_probabilities = np.array([item['probabilities'] for item in validation_probabilities])
+
+        training_log_loss = metrics.log_loss(training_targets, training_probabilities)
+        validation_log_loss = metrics.log_loss(validation_targets, validation_probabilities)
+        # Occasionally print the current loss.
+        print("  period %02d : %0.2f" % (period, training_log_loss))
+        # Add the loss metrics from this period to our list.
+        training_log_losses.append(training_log_loss)
+        validation_log_losses.append(validation_log_loss)
+    print("Model training finished.")
+
+    # Output a graph of loss metrics over periods.
+    plt.ylabel("LogLoss")
+    plt.xlabel("Periods")
+    plt.title("LogLoss vs. Periods")
+    plt.tight_layout()
+    plt.plot(training_log_losses, label="training")
+    plt.plot(validation_log_losses, label="validation")
+    plt.legend()
+    plt.show()
+
+    return linear_classifier
+
+
+linear_classifier = train_linear_classifier_model(
+    learning_rate=0.03,
+    regularization_strength=0.1,
+    steps=300,
+    batch_size=100,
+    feature_columns=construct_feature_columns(),
+    training_examples=training_examples,
+    training_targets=training_targets,
+    validation_examples=validation_examples,
+    validation_targets=validation_targets
+)
+
+print("Model size:", model_size(linear_classifier))
