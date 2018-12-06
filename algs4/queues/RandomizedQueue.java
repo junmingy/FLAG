35a1b6684452fff6560b0a61e7b2cde44d115d84
phdsky <huid.peng@gmail.com>
2018-12-06 23:41:57 +0800

    GGCFCMD

--- algs4/queues/RandomizedQueue.java
+++ algs4/queues/RandomizedQueue.java
@@ -7,15 +7,86 @@
 
 import java.util.Iterator;
 
-public class RandomizedQueue {
-    public RandomizedQueue()                 // construct an empty randomized queue
-    public boolean isEmpty()                 // is the randomized queue empty?
-    public int size()                        // return the number of items on the randomized queue
-    public void enqueue(Item item)           // add the item
-    public Item dequeue()                    // remove and return a random item
-    public Item sample()                     // return a random item (but do not remove it)
-    public Iterator<Item> iterator()         // return an independent iterator over items in random order
+public class RandomizedQueue<Item> implements Iterable<Item> {
 
+    /**
+     * @brief Construct an empty randomized queue
+     */
+    public RandomizedQueue() {
+
+    }
+
+    /**
+     * @brief Is the randomized queue empty?
+     * @return
+     */
+    public boolean isEmpty() {
+        return false;
+    }
+
+    /**
+     * @brief Return the number of items on the randomized queue
+     * @return
+     */
+    public int size() {
+        return -1;
+    }
+
+    /**
+     * @brief Add the item
+     * @param item
+     */
+    public void enqueue(Item item) {
+
+    }
+
+    /**
+     * @brief Remove and return a random item
+     * @return
+     */
+    public Item dequeue() {
+        Item item = null;
+        return item;
+    }
+
+    /**
+     * @brief Return a random item (but do not remove it)
+     * @return
+     */
+    public Item sample() {
+        Item item = null;
+        return item;
+    }
+
+    /**
+     * @brief Return an independent iterator over items in random order
+     * @return
+     */
+    public Iterator<Item> iterator() {
+        return new ArrayIterator();
+    }
+
+    /**
+     * @brief
+     */
+    private class ArrayIterator implements Iterator<Item> {
+        public boolean hasNext() {
+            return false;
+        }
+
+        public Item next() {
+            return null;
+        }
+
+        public void remove() {
+
+        }
+    }
+
+    /**
+     * @brief Unit testing (optional)
+     * @param args
+     */
     public static void main(String[] args) {
 
     }
